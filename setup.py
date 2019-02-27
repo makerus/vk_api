32a24cd7a39109edd303d8f3124997b9bb75b865
boroda34 <katagarov@yandex.ru>
2019-02-27 21:46:05 +0400

    обновление версии

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='vk_api_boroda34',
-      version='0.2',
+      version='0.3',
       description='Библиотека для работы с API социальной сети ВКонтакте',
       author='boroda34',
       url='https://github.com/makerus',
