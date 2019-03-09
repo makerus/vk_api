85c30e9bb85df59e09d9cf6a65911588bd988985
Egor Katagarov <katagarov2018@mail.ru>
2019-03-09 20:38:23 +0400

    добавление функционала long poll и возможность расширения с помощью команд

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ pip install vk-api-boroda34
 ```python
 from vk_api.core.api import VkApi
 
-# Инициализация объекта VK API
+# Инициализация объекта VK API для пользователя
 api = VkApi.login({
     'client_id': config.CLIENT_ID,
     "scope": config.SCOPE,
@@ -20,20 +20,50 @@ api = VkApi.login({
     "max_timeout": config.MAX_TIMEOUT
 })
 
+# Инициализация объекта VK API для группы
+bot = VkApi({
+    'client_id': config.CLIENT_ID,
+    'scope': config.SCOPE_GROUP,
+    'api_v': config.VERSION_API,
+    'token': config.TOKEN_GROUP,
+    'max_timeout': config.MAX_TIMEOUT,
+    'group_id': config.GROUP_ID
+})
+
 # Авторизация и получение токена
 api.get_token(api)
+
+# Опциональные команды
+
+# Работа longPool (запускает отдельный поток)
+api.long_poll()
+
+# Инициализация команд
+api.register_commands(ClassName)
+# Регистрация символа команды
+api.register_symbol_command('#')
+# Регистрация символа ответа
+api.register_symbol_answer('!')
+
+# Дальше может быть расположен любой код
 
 ```
 
 Для работы библиотеки потребуется файл ```config.py```
 Содержание этого файла должно быть следующим:
 ```python
-CLIENT_ID = ''  # ID приложения
-SCOPE = 'friends,photos,video,status,messages,wall,offline,docs,groups'  # Здесь указаны права доступа
-LOGIN = ''  # Номер телефона или email от профиля бота
-PASSWORD = ''  # Пароль от профиля бота
-VERSION_API = '5.92'  # Версия API
+CLIENT_ID = 000000  # ID приложения
+VERSION_API = "5.92"  # Версия API
 LEVEL_LOG = 0  # Уровень ошибок 0 - ВСЕ, 1 - ТОЛЬКО INFO и ERRORS, 2 - ТОЛЬКО ERRORS
-MAX_TIMEOUT = 3 # Время таймаута (время отправки запросов к API ВКонтакте) в секундах
+MAX_TIMEOUT = 3  # Таймаут от 1 до 3 секунд
 
+# Для пользоавтеля
+SCOPE_USER = "friends,photos,video,status,wall,offline,docs,groups"  # Здесь указаны права доступа
+LOGIN = ""  # Номер телефона или email от профиля бота
+PASSWORD = ""  # Пароль от профиля бота
+
+# Для группы
+TOKEN_GROUP = ""  # токен
+SCOPE_GROUP = "stories,photos,app_widget,messages,docs,manage"  # Здесь указаны права доступа
+GROUP_ID = 000000
 ```
--- vk_api/api.py
+++ vk_api/api.py
@@ -1,6 +1,9 @@
 import json
 import time
 import random
+import threading
+import signal
+import os
 
 from vk_api.http_util import HttpUtil
 from urllib.parse import urlparse, parse_qs
@@ -15,22 +18,27 @@ class VkApi:
         Создание объекта API
         :param options:
         """
+        self.commands = []
         self.options = options
         self.authorize_url = "https://oauth.vk.com/authorize"
         self.redirect_uri = "http://oauth.vk.com/blank.html"
         self.action = "https://login.vk.com/?act=login&soft=1&utf8=1"
         self.method_action = "https://api.vk.com/method/"
         self.auth_config = {
-                "client_id": self.options['client_id'],
-                "redirect_uri": self.redirect_uri,
-                "display": "mobile",
-                "response_type": "token",
-                "scope": self.options['scope'],
-                "v": self.options['api_v'],
-                "revoke": 1
-            }
+            "client_id": self.options['client_id'],
+            "redirect_uri": self.redirect_uri,
+            "display": "mobile",
+            "response_type": "token",
+            "scope": self.options['scope'],
+            "v": self.options['api_v'],
+            "revoke": 1
+        }
         self.token = None
         self.logger = Logger('vk_api')
+        self.symbol_command = '@'
+        self.symbol_answer = '#'
+
+        signal.signal(signal.SIGINT, self.handler_signal)
 
     def get_token(self, api):
         """
@@ -38,8 +46,24 @@ class VkApi:
         Или его повторное получение, в случае его отсутствия
         :return:
         """
-        token = Tokenizer('.token', api)
-        token.token_init()
+        if 'token' not in self.options:
+            token = Tokenizer('.token', api)
+            token.token_init()
+        else:
+            self.logger.log('Авторизация черезе токен группы')
+            self.set_token(self.options['token'])
+
+    def set_token(self, token):
+        """
+        Установка токена
+        :param token:
+        :return:
+        """
+        if token:
+            self.token = token
+        else:
+            self.logger.error('Проверьте настройки конфигурации!')
+            exit()
 
     def login(self):
         """
@@ -59,21 +83,15 @@ class VkApi:
         response_get_token = http.get(action)
         url_token = urlparse(response_get_token['url'])
         try:
-            self.token = parse_qs(url_token.fragment, encoding='utf8')['access_token'][0]
+            self.set_token(parse_qs(url_token.fragment, encoding='utf8')['access_token'][0])
         except KeyError:
-            try:
-                json_response = json.loads(response_get_token['content'])
-                self.logger.error(json_response)
-            except json.decoder.JSONDecodeError:
-                self.logger.error("Проверьте данные в конфигурации")
-                exit()
+            self.logger.error(json.loads(response_get_token['content']))
 
         if self.token is not None:
             self.logger.log('Авторизация завершена')
         else:
             self.logger.error('Ошибка авторизации')
-
-        exit()
+            exit()
 
     def query(self, name, params):
         """
@@ -87,12 +105,132 @@ class VkApi:
         for k, v in params.items():
             query_param.append(str(str(k) + "=" + str(v)))
 
-        url = "https://api.vk.com/method/" + name + "?" + '&'.join(query_param) +\
+        url = "https://api.vk.com/method/" + name + "?" + '&'.join(query_param) + \
               "&access_token=" + self.token + "&v=" + self.options['api_v']
         query = http.get(url)
         response = json.loads(query['content'])
         time.sleep(random.randint(1, self.options['max_timeout']))
+
         if 'error' in response:
             return json.loads(query['content'])['error']
         else:
             return json.loads(query['content'])['response']
+
+    def get_long_poll(self):
+        """
+        Инициализация getLongPoll
+        :return:
+        """
+        pts = self.query('groups.getLongPollServer', {'group_id': self.options['group_id']})
+        return pts
+
+    def get_ts(self, server, key, ts):
+        """
+        Получение TS для API getLongPoll
+        :param server:
+        :param key:
+        :param ts:
+        :return:
+        """
+        http = HttpUtil()
+        url_server = server + '?act=a_check&key=' + key + '&ts=' + ts + '&wait=25'
+        response = http.get(url_server)
+        return json.loads(response['content'])
+
+    def init_logn_poll(self):
+        """
+        Реализация getLongPool подключения
+        :return:
+        """
+        pts = self.get_long_poll()
+        key = pts['key']
+        server = pts['server']
+
+        while True:
+            pts = self.get_ts(server, key, pts['ts'])
+            self.middleware(pts['updates'])
+
+    def long_poll(self):
+        """
+        Инициализация и запуск процесса LongPoll
+        :return:
+        """
+        thread = threading.Thread(target=self.init_logn_poll, name='long_poll')
+        thread.start()
+        self.logger.log('Long pull запущен')
+
+    def middleware(self, items):
+        """
+        Обработчик поступающих событий
+        :param items:
+        :return:
+        """
+        if items is not None:
+            for item in items:
+                self.is_command(item)
+
+    def handler_signal(self, signal_proc, frame):
+        """
+        Уничтожение процесса
+        :param signal_proc:
+        :param frame:
+        :return:
+        """
+        pid = os.getpid()
+        os.kill(pid, signal.SIGKILL)
+
+    def register_commands(self, *commands):
+        """
+        Регистрация комманд
+        :param commands:
+        :return:
+        """
+
+        for cmd in commands:
+            command = cmd()
+            self.commands.append({'name': command.name, 'event': command.event,
+                                  'result': command.result, 'filter': command.filter})
+
+    def register_symbol_command(self, symbol):
+        """
+        Регистрация символа команды
+        :param symbol:
+        :return:
+        """
+        self.symbol_command = symbol
+
+    def register_symbol_answer(self, symbol):
+        """
+        Регистрация символа ответа
+        :param symbol:
+        :return:
+        """
+        self.symbol_answer = symbol
+
+    def is_command(self, item):
+        """
+        Проверка: является ли сообщение командой
+        :param item:
+        :return:
+        """
+        for cmd in self.commands:
+            event = item['type']
+
+            if event == cmd['event']:
+                if cmd['filter'](item['object'], self.symbol_command, self.symbol_answer):
+                    resposne = cmd['result'](self, item)
+                    if resposne:
+                        self.replay(resposne, item)
+
+    def replay(self, text, item):
+        """
+        Ответ, если он предусмотрен командой
+        :param text:
+        :param item:
+        :return:
+        """
+        peer_id = item['object']['peer_id']
+        random_id = random.randint(11111111111, 99999999999)
+        message = text
+        self.query('messages.send', {'peer_id': peer_id, 'random_id': random_id, 'message': message,
+                                     'group_id': self.options['group_id']})
